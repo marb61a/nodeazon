8907877b503f7d434ce3e22446a862e6a0402e45
marb <marb61a@gmail.com>
2016-01-21 18:57:36 +0000

    updating dependencies and setting up server js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require("express");
+var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect(secret.database, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
+  }
+});
+
+// Middleware
+app.use(morgan('dev'));
 
 app.listen(process.env.PORT, process.env.IP, function(err){
     if (err) throw err;
