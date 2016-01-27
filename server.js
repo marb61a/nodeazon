9c2bc0e4bd6231cf513464a217fbf059da9d44a1
marb <marb61a@gmail.com>
2016-01-27 11:30:45 +0000

    updated server js and folder layout

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
 
 var app = express();
 
@@ -13,7 +15,16 @@ mongoose.connect(secret.database, function(err) {
 });
 
 // Middleware
+app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
 
 app.listen(process.env.PORT, process.env.IP, function(err){
     if (err) throw err;
