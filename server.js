a5229aa45751c0fafa3d790d379ae8ab42309ff6
marb <marb61a@gmail.com>
2016-02-05 11:06:06 +0000

    updated server js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-var express = require("express");
+var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
+var MongoStore = require('connect-mongo/es5')(session);
 var passport = require('passport');
+
+var secret = require("./config/secret");
+var User = require('./models/user');
 
 var app = express();
 
@@ -20,12 +28,30 @@ app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: secret.secretKey,
+  store: new MongoStore({ url: secret.database, autoReconnect: true})
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
 
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
 var mainRoutes = require('./routes/main');
 var userRoutes = require('./routes/user');
+
+app.use(mainRoutes);
+app.use(userRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(err){
     if (err) throw err;
