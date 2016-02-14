e98f3ff6a440587131e8cdd17e628859a2188b43
marb <marb61a@gmail.com>
2016-02-14 19:05:57 +0000

    updated user model

--- models/user.js
+++ models/user.js
@@ -36,5 +36,16 @@ UserSchema.pre('save', function(next){
   });
 });
 
+// Compare user entered password to stored password
+UserSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.methods.gravatar = function(size) {
+  if (!this.size) size = 200;
+  if (!this.email) return 'https://gravatar.com/avatar/?s' + size + '&d=retro';
+  var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';
+};
 
 module.exports = mongoose.model('User', UserSchema);
