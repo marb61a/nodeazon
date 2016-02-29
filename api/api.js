ed6e93766d4a750083907649e005f1a1df1280dc
marb <marb61a@gmail.com>
2016-02-29 14:03:26 +0000

    added initial api js content

---
+++ api/api.js
@@ -0,0 +1,15 @@
+var router = require('express').Router();
+var async = require('async');
+var faker = require('faker');
+var Category = require('../models/category');
+var Product = require('../models/product');
+
+router.post('/search', function(req, res, next){
+    
+});
+
+router.get('/:name', function(req, res, next){
+    
+});
+
+module.exports = router;
