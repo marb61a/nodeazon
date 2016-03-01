0dc0a5f9e6e5a3be021f64df7cdcf63f7d58dd06
marb <marb61a@gmail.com>
2016-03-01 18:38:24 +0000

    add paginate function to routes

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,26 @@ var router = require('express').Router();
 var User = require('../models/user');
 var Product = require('../models/product');
 
-
+function paginate (req, res, next){
+  var perPage = 9;
+  var page = req.params.page;
+  
+  Product
+    .find()
+      .skip( perPage * page)
+      .limit( perPage )
+      .populate('category')
+      .exec(function(err, products) {
+        if (err) return next(err);
+        Product.count().exec(function(err, count) {
+          if (err) return next(err);
+          res.render('main/product-main', {
+            products: products,
+            pages: count / perPage
+          });
+        });
+      });
+}
 
 router.get('/', function(req, res, next) {
   res.render('main/home');
