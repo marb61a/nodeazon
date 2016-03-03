c86d33b766438e063cbeb90ccb2be8676532656d
marb <marb61a@gmail.com>
2016-03-03 13:50:19 +0000

    added post and get search routes

--- routes/main.js
+++ routes/main.js
@@ -48,6 +48,30 @@ stream.on('error', function(err) {
   console.log(err);
 });
 
+
+router.post('/search', function(req, res, next){
+  res.redirect('/search?=' + req.body.q);
+});
+
+router.get('/search', function(req, res, next){
+  if(req.query.q){
+    Product.search({
+      query_string: { query: req.query.q}
+    }, function(err, results) {
+      results:
+      if (err) return next(err);
+      var data = results.hits.hits.map(function(hit) {
+        return hit;
+      });
+      res.render('main/search-result', {
+        query: req.query.q,
+        data: data
+      });
+    });
+  }
+});
+
+
 router.get('/', function(req, res, next) {
 
   if (req.user) {
