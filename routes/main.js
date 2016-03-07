fef9b164c6a525f4d9a327b34e5f5a1b84e45fce
marb <marb61a@gmail.com>
2016-03-07 14:09:25 +0000

    added get product and products routes to main

--- routes/main.js
+++ routes/main.js
@@ -90,5 +90,27 @@ router.get('/about', function(req, res) {
   res.render('main/about');
 });
 
+router.get('/products/:id', function(req, res, next){
+  Product
+    .find({ category: req.params.id })
+    .populate('category')
+    .exec(function(err, products){
+      if (err) return next(err);
+      res.render('main/category', {
+        products: products
+      });
+    });
+    
+});
+
+router.get('/product/:id', function(req, res, next) {
+    Product.findById({_id: req.params.id}, function(err, product){
+        if (err) return next(err);
+        res.render('main/product', {
+          product: product
+      });
+    });
+});
+
 
 module.exports = router;
