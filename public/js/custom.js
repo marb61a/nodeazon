1adf48f35fa39e0cb3d15af1b4325af7a35c802f
marb <marb61a@gmail.com>
2016-03-21 14:24:45 +0000

    added click plus function to custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -34,9 +34,22 @@ $(function() {
         },
 
         error: function(error) {
-            console.log(err);
+            console.log(error);
       }
     });
+  });
+  
+  $(document).on('click', '#plus', function(e){
+        e.preventDefault();
+        var priceValue = parseFloat($('#priceValue').val());
+        var quantity = parseInt($('#quantity').val());
+    
+        priceValue += parseFloat($('#priceHidden').val());
+        quantity += 1;
+    
+        $('#quantity').val(quantity);
+        $('#priceValue').val(priceValue.toFixed(2));
+        $('#total').html(quantity);
   });
   
 })
