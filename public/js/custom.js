a96c7c00cf168024715d24e0296074d7cced02d3
marb <marb61a@gmail.com>
2016-03-23 14:56:51 +0000

    added options to custom js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,28 @@
 $(function() {
+    
+    var opts = {
+        lines: 13 // Number of lines to draw
+        , length: 27 // Line length
+        , width: 30 // Line thickness
+        , radius: 42 // The radius of the inner circle
+        , scale: 1 // Scales overall size of the spinner
+        , corners: 1 // Corner roundness (0..1)
+        , color: '#000' // #rgb
+        , opacity: 0.25 // Lines Opacity
+        , rotate: 0 // The rotation offset
+        , direction: 1 // 1: clockwise, -1: counterclockwise
+        , speed: 1 // Rounds per second
+        , trail: 60 // Afterglow percentage
+        , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+        , zIndex: 2e9 // The z-index (defaults to 2000000000)
+        , className: 'spinner' // The CSS class to assign to the spinner
+        , top: '50%' // Top position relative to parent
+        , left: '50%' // Left position relative to parent
+        , shadow: false // Whether to render a shadow
+        , hwaccel: false // Whether to use hardware acceleration
+        , position: 'absolute' // Positioning of Element
+    };
+    
   $('#search').keyup(function() {
     var search_term = $(this).val();
 
