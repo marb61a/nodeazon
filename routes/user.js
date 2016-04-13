05941b78acfd3ce7ca9505f3c8b76956ff19b27d
marb <marb61a@gmail.com>
2016-04-13 12:47:40 +0000

    updated signup post function

--- routes/user.js
+++ routes/user.js
@@ -29,30 +29,41 @@ router.get('/signup', function(req, res, next) {
 });
 
 router.post('/signup', function(req, res, next) {
-  var user = new User();
+   async.waterfall([
+    function(callback) {
+      var user = new User();
 
-  user.profile.name = req.body.name;
-  user.email = req.body.email;
-  user.password = req.body.password;
-  user.profile.picture = user.gravatar();
+      user.profile.name = req.body.name;
+      user.email = req.body.email;
+      user.password = req.body.password;
+      user.profile.picture = user.gravatar();
 
-  User.findOne({ email: req.body.email }, function(err, existingUser) {
+      User.findOne({ email: req.body.email }, function(err, existingUser) {
 
-    if (existingUser) {
-      req.flash('errors', 'Account with that email address already exists');
-      return res.redirect('/signup');
-    } else {
-      user.save(function(err, user) {
+        if (existingUser) {
+          req.flash('errors', 'Account with that email address already exists');
+          return res.redirect('/signup');
+        } else {
+          user.save(function(err, user) {
+            if (err) return next(err);
+            callback(null, user);
+          });
+        }
+      });
+    },
+
+    function(user) {
+      var cart = new Cart();
+      cart.owner = user._id;
+      cart.save(function(err) {
         if (err) return next(err);
-
         req.logIn(user, function(err) {
           if (err) return next(err);
           res.redirect('/profile');
-
         });
       });
     }
-  });
+  ]);
 });
 
 router.get('/logout', function(req, res, next) {
